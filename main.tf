1e8de55066165825b605d2a234ff48fc6cbf1e69
Diego Martinez <diegomab9@gmail.com>
2024-07-12 22:18:18 -0300

    Prueba Despliegue s3 Terraform 3

--- main.tf
+++ main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-variable "bucket_name" {
-  type        = string
-  description = "The name of the S3 bucket"
-}
-
 resource "aws_s3_bucket" "my_app_bucket" {
   bucket = var.bucket_name
   acl    = "private"
