d19bdcd7bc63c6fc74633d09f405332b076931fe
Diego Martinez <diegomab9@gmail.com>
2024-07-12 23:06:55 -0300

    Prueba Despliegue s3 Terraform 6

--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+
+variable "branch_name" {
+  description = "Name of the Git branch"
+  type        = string
+}
